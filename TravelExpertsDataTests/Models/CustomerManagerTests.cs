2769291c384a7dfd93898cb59df93ce54f37979c
Sujani Wijesundera <stw0327@p2es.com>
2022-03-10 11:35:29 -0700

    Add a test class

---
+++ TravelExpertsDataTests/Models/CustomerManagerTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TravelExpertsData.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Sdk;
+
+namespace TravelExpertsData.Models.Tests
+{
+    [TestClass()]
+    public class CustomerManagerTests
+    {
+        [TestMethod()]
+        public void GetCustomerIdTest()
+        {
+            int custId = 1;
+            Assert.Equals(1, custId);
+        }
+    }
+}
