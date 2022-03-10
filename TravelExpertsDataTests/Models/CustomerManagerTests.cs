832d9f766c6a8224da4b5c5cdd24762cf448b8e2
Sujani Wijesundera <stw0327@p2es.com>
2022-03-10 13:06:40 -0700

    Test fail test

--- TravelExpertsDataTests/Models/CustomerManagerTests.cs
+++ TravelExpertsDataTests/Models/CustomerManagerTests.cs
@@ -24,7 +24,7 @@ namespace TravelExpertsData.Models.Tests
         public void GetCustomerName()
         {
             string customerName = "Sujani";
-            Assert.AreEqual("Sujani", customerName);
+            Assert.AreEqual("sujani", customerName);
             System.Console.WriteLine("Ok!!");
         }
     }
